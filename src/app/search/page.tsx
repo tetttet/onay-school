1e43520fca58815a3960c9d40216927f7206d2cb
Turan-Yahya Gazizuly <turan-yahyagazizuly@Turan-Yahyas-MacBook-Pro.local>
2025-06-28 04:42:01 +0300

    suspense

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,24 +1,11 @@
-"use client";
+// app/search/page.tsx
+import SearchPage from "@/components/demo/SearchClient";
+import { Suspense } from "react";
 
-import { useSearchParams } from "next/navigation";
-
-export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
-
+export default function Page() {
   return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Результаты поиска</h1>
-
-      {query ? (
-        <p className="text-muted-foreground">
-          Поиск по запросу: <strong>{query}</strong>
-        </p>
-      ) : (
-        <p className="text-muted-foreground">Введите запрос для поиска.</p>
-      )}
-
-      {/* Здесь будет логика поиска/фильтрации */}
-    </main>
+    <Suspense fallback={<div>Загрузка...</div>}>
+      <SearchPage />
+    </Suspense>
   );
 }
